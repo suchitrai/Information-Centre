377896f9cafb9ba2b1e8b23f01360c48f4c36cde
Sakthi-M <35755986+Sakthi-M@users.noreply.github.com>
2018-02-15 10:43:27 +0000

    Update LoginIT.java

--- src/test/java/LoginIT.java
+++ src/test/java/LoginIT.java
@@ -1,16 +1,27 @@
 
+
+import org.testing.annotations.Test;
+
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+ 
+
+ 
 
 public class LoginIT {
-	public static String a;
-	public static String b;
-	public static void main(String[] args) throws InterruptedException {
+
+               
+		public WebDriver driver;
+		public static String a;
+		public static String b;
+		public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver=new InternetExplorerDriver();
-		driver.get("https://sjavaapp.azurewebsites.net/InformationCentre/Login.jsp");
+		driver.get("http://localhost:8008/InformationCentre/Login.jsp");
 		Thread.sleep(2000);
 		driver.findElement(By.name("username")).sendKeys("admin");
 		driver.findElement(By.name("password")).sendKeys("admin");
@@ -18,18 +29,27 @@ public class LoginIT {
 		driver.findElement(By.id("click")).click();
 		a=driver.getTitle();
 		b=driver.findElement(By.id("name")).getText();
+			
 		if (b.equalsIgnoreCase("Hello admin ,")) {
-			System.out.println("Login Test Passed");
+		System.out.println("Login Test Passed");
 		}
 		else{
-			System.out.println("Login Test  Failed");
+		System.out.println("Login Test  Failed");
 		}
+
+
 		if (a.equalsIgnoreCase("CICD:Home page")) {
-			System.out.println("Home page Verified");
+		System.out.println("Home page Verified");
 		}
 		else{
-			System.out.println("Login Test Failed");
+		System.out.println("Login Test Failed");
 		}
-	}
+
 
 }
+
+ 
+
+}
+
+
